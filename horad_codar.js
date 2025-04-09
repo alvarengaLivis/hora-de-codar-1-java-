8105925728e205254e230537371180519155cd86
Livia Alvarenga <livia.rib.alv@gmail.com>
2025-04-09 19:20:34 -0300

    Add files via upload

---
+++ horad_codar.js
@@ -0,0 +1,17 @@
+
+// Escreva um programa que crie uma variável chamada "nome_do_carro" e atribua-a um valor "Fusca". Exiba-a ao usuário.
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hora de codar</title>
+    </head>
+<body>
+    
+    
+
+</body>
+</html>
